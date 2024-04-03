b3092d9d4dd94f8528003157cccb77f255e942cd
João Constantino Caetano <161314952+gauchofbpa@users.noreply.github.com>
2024-04-03 09:33:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function funcaoMudarTamanho() {
+    document.getElementById("test").style.fontSize = "35px";
+}
+function funcaoMudarOBack() {
+    document.getElementById("test").style.color = "#ff0000";
+    document.getElementById("test").style.backgroundColor = "#00ff00";
+}
+function conta() {
+    document.getElementById("test").innerHTML = 5 + 8;
+}
